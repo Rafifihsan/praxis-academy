76a2e6411433363593d75b1586f9d07d4a73b47d
Rafifihsan <61154392+Rafifihsan@users.noreply.github.com>
2020-03-27 21:21:31 -0700

    Add files via upload

---
+++ 03-02/api/script.js
@@ -0,0 +1,5 @@
+$.getJSON('pizza.json', function(data){
+	let menu = data.menu;
+	$.each(menu, function(i,data){
+		$('#daftar-menu').append('<div class="col-md-4"> <div class="card mb-3"><img src="img/pizza/'+data.gambar+'" class="card-img-top" ><div class="card-body"><h5 class="card-title">'+data.nama+'</h5><p class="card-text">'+data.deskripsi+'</p><a href="#" class="btn btn-primary">'+data.harga+'</a></div></div> </div>')})
+});
